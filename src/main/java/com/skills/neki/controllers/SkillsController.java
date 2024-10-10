6f5344cd70af4c8d00bd4c84ecafe8dc26816ece
MatheusMelloDev <165337919+MatheusMelloDev@users.noreply.github.com>
2024-10-10 18:04:10 -0300

    Finalizando todos os métodos

--- src/main/java/com/skills/neki/controllers/SkillsController.java
+++ src/main/java/com/skills/neki/controllers/SkillsController.java
@@ -10,9 +10,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,28 +78,25 @@ public class SkillsController {
         return ResponseEntity.ok(skills);
     }
 
-    @PatchMapping("/alterar_infos/{id}")
+    @PutMapping("/alterar_infos/{id}")
     public ResponseEntity<Skills> updateSkillInfo(
         @PathVariable Long id,
-        @RequestParam String nome,
-        @RequestParam String descricao,
-        @RequestParam String tecnologia,
-        @RequestParam String nivel) {
+        @RequestBody Skills updatedSkill) {
 
         // Busca a skill pelo ID
-        Skills skills = skillsRepository.findById(id)
+        Skills existingSkill = skillsRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("Skill não encontrada com id: " + id));
 
         // Atualiza os campos
-        skills.setNome(nome);
-        skills.setDescricao(descricao);
-        skills.setTecnologia(tecnologia);
-        skills.setNivel(nivel);
+        existingSkill.setNome(updatedSkill.getNome());
+        existingSkill.setDescricao(updatedSkill.getDescricao());
+        existingSkill.setTecnologia(updatedSkill.getTecnologia());
+        existingSkill.setNivel(updatedSkill.getNivel());
 
         // Salva as alterações no banco de dados
-        skillsRepository.save(skills);
+        skillsRepository.save(existingSkill);
 
-        return ResponseEntity.ok(skills);
+        return ResponseEntity.ok(existingSkill);
     }
     
     @DeleteMapping("/deletar_skills/{id}")
