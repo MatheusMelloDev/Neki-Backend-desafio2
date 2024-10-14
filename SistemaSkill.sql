38b7136ebe3b2b678a631c96e0eac4fa65acb7e5
MatheusMelloDev <165337919+MatheusMelloDev@users.noreply.github.com>
2024-10-14 09:12:33 -0300

    Inserindo o script de população do banco

---
+++ SistemaSkill.sql
@@ -0,0 +1,10 @@
+INSERT INTO skills (nome, descricao, tecnologia, nivel)
+VALUES
+('Desenvolvedor Backend', 'Desenvolvedor API', 'Java', 'Básico'),
+('Desenvolvedor FrontEnd', 'Designer UI/UX', 'React', 'Intermediário'),
+('Desenvolvedor FullStack', 'Desing de Banco de Dados', 'Node.js', 'Avançado'),
+('Desenvolvedor Backend', 'Desenvolvedor API', 'SQL', 'Básico'),
+('Desenvolvedor FrontEnd', 'Designer UI/UX', 'Java', 'Intermediário'),
+('Desenvolvedor FullStack', 'Desing de Banco de Dados', 'React', 'Avançado'),
+('Desenvolvedor Backend', 'Desenvolvedor API', 'Node.js', 'Básico'),
+('Desenvolvedor FrontEnd', 'Designer UI/UX', 'SQL', 'Intermediário');
